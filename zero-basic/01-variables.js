87fae5545b1f7908e5ee0fe7899f93e4c9a65357
kumudbalaa <balakumud551@gmail.com>
2025-03-19 17:37:28 +0000

    typedata

---
+++ zero-basic/01-variables.js
@@ -0,0 +1,13 @@
+const accountId = 123455;
+let accountemail = "kumud@gmail.com";
+var accountpassword = "2345";
+
+accountemail="bhanu@gmail.com";
+accountpassword ="776567";
+
+console.log(accountId);
+
+//  prefer not use var
+// because of issue in block scope and functional scope
+
+console.table([accountId,accountemail,accountpassword])
