24783887c97975e6b01a24b2869e94e58d73b83a
kumudbalaa <balakumud551@gmail.com>
2025-03-19 17:28:21 +0000

    datatypes

---
+++ zero-basic/02_datatype.js
@@ -0,0 +1,20 @@
+
+
+let name = "kumud";
+let age = 20;
+let isloggedin = false;
+
+// number => 2 to powetr 53 
+//  bigint 
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => abhi value ASSine ni hue hai
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);  // object hai null
+console.log(typeof undefined); // undefined ka type undefined hi hai
+
