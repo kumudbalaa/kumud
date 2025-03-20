097364b3c4132861d1984adfc25a1ea40d3b63ed
kumud bala <balakumud551@gmail.com>
2025-03-20 14:44:21 +0000

    conversion of data types

---
+++ zero-basic/03-conversiopr.js
@@ -0,0 +1,30 @@
+let score = "kumud"
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// const {score} = 
+
+let valueInnumber = Number(score )
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+// "33" => 33
+// "33qdrt" => Nan 
+// "true" => 1; false => 0
+
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true; 0 => false
+// "" => false
+// "kumd" => true
+
+
+
+let someNumber = 44
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
