9631828d693406021d298c657b6de85fbd851d8d
kumud bala <balakumud551@gmail.com>
2025-03-27 16:47:58 +0000

    dates & time

---
+++ zero-basic/07-dates.js
@@ -0,0 +1,21 @@
+//   dates 
+
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleDateString());
+
+// let mycreateddate = new Date(2023,0,23);
+// let mycreateddate = new Date(2023,0,23 , 5 ,4 );
+// console.log(mycreateddate.toDateString());
+
+// let mycreateddate = new Date("01-14-2023");
+// console.log(mycreateddate.getTime());
+
+// let mytimestamp =( Date.now())
+// console.log(mytimestamp);
+
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getDay () ) ;
